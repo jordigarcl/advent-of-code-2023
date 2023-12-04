be8cc1fa17052fe0018b9c4ed78d6fba30075e3d
Jordi García <jgarcialisson@newrelic.com>
2023-12-04 11:38:37 +0100

    Day03-02

--- app/src/test/kotlin/com/jordigarcial/Day03.kt
+++ app/src/test/kotlin/com/jordigarcial/Day03.kt
@@ -54,9 +54,6 @@ class Day03 {
         val foundNumbers = mutableListOf<Pair<Int, Line2D>>()
         val foundSymbols = mutableListOf<Point2D>()
 
-        val maxX = input[0].length - 1
-        val maxY = input.size - 1
-
         // E.g. "467..114.."
         for ((y, inputLine) in input.withIndex()) {
             // E.g. "467"
@@ -101,15 +98,87 @@ class Day03 {
         return distance <= 1.4142135623730951;
     }
 
+    /**
+     * --- Part Two ---
+     *
+     * The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+     *
+     * You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+     *
+     * Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+     *
+     * The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+     *
+     * This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+     *
+     * Consider the same engine schematic again:
+     *
+     * 467..114..
+     *
+     * ...*......
+     *
+     * ..35..633.
+     *
+     * ......#...
+     *
+     * 617*......
+     *
+     * .....+.58.
+     *
+     * ..592.....
+     *
+     * ......755.
+     *
+     * ...$.*....
+     *
+     * .664.598..
+     *
+     * In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+     *
+     * What is the sum of all of the gear ratios in your engine schematic?
+     */
+    fun solvePuzzle2(input: List<String>) : Int {
+        val foundNumbers = mutableListOf<Pair<Int, Line2D>>()
+        val foundSymbols = mutableListOf<Point2D>()
 
+        // E.g. "467..114.."
+        for ((y, inputLine) in input.withIndex()) {
+            // E.g. "467"
+            regexNumber.findAll(inputLine).forEach { matchResult ->
+                val number = matchResult.value.toInt()
+                val coords = Line2D.Double(
+                    Point2D.Double(matchResult.range.first.toDouble(), y.toDouble()),
+                    Point2D.Double(matchResult.range.last.toDouble(), y.toDouble())
+                )
+                println("Found $number at ${coords.stringify()}")
+                foundNumbers += number to coords
+            }
 
+            // E.g. "*"
+            regexSymbols.findAll(inputLine).forEach { matchResult ->
+                val coord = Point2D.Double(matchResult.range.first.toDouble(), y.toDouble())
+                println("Found symbol at ${coord.stringify()}")
+                foundSymbols +=  coord
+            }
+        }
 
-//    fun solvePuzzle2(input: List<String>) : Int = input
-//        .map { inputLine -> evaluateMinimumSetOfCubesForGame(inputLine) }
-//        .sumOf { minimumSetOfCubes ->
-//            minimumSetOfCubes.values.reduce { acc, i -> acc * i }
-//        }
-//
+        var gearRatiosSum = 0
+        for (symbol in foundSymbols) {
+            val gearNumbers = mutableListOf<Int>()
+            for (number in foundNumbers) {
+                if (symbol.isAdjacentToLine(number)) {
+                    println("Number ${number.first} on ${number.second.stringify()} is adjacent to symbol on ${symbol.stringify()}")
+                    gearNumbers += number.first
+                }
+            }
+            if (gearNumbers.size == 2) {
+                println("Gear detected: ${symbol.stringify()}")
+                gearRatiosSum += gearNumbers[0] * gearNumbers[1]
+            }
+        }
+        return gearRatiosSum
+    }
+
 
     @ParameterizedTest
     @CsvSource(
@@ -123,15 +192,15 @@ class Day03 {
         assertEquals(Integer.parseInt(result), solvePuzzle1(input))
     }
 
-//    @ParameterizedTest
-//    @CsvSource(
-//        value = [
-//            "/day02-example1.txt,2286",
-//            "/day02-input1.txt,71274"
-//        ]
-//    )
-//    fun testPuzzle2(inputFile: String, result: String) {
-//        val input = IOUtils.readLines(javaClass.getResourceAsStream(inputFile), "UTF-8") as List<String>
-//        assertEquals(Integer.parseInt(result), solvePuzzle2(input))
-//    }
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "/day03-example1.txt,467835",
+            "/day03-input1.txt,0"
+        ]
+    )
+    fun testPuzzle2(inputFile: String, result: String) {
+        val input = IOUtils.readLines(javaClass.getResourceAsStream(inputFile), "UTF-8") as List<String>
+        assertEquals(Integer.parseInt(result), solvePuzzle2(input))
+    }
 }
